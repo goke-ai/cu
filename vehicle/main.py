88cd42eda8d598c8cc8ad2c92711f21faa16ffde
Olagoke Oladokun <gokeladokun@hotmail.com>
2021-03-19 19:58:16 +0100

    Add project files

---
+++ vehicle/main.py
@@ -0,0 +1,4 @@
+from sklearn.neural_network import MLPRegressor
+from sklearn.datasets import make_regression
+from sklearn.model_selection import train_test_split
+X, y = make_regression(n_samples=200, random_state=1)
