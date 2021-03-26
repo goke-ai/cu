5994d3658d28676f8b6538f7f8e9cda02c288181
Gole Oladokun <gokeladokun@gmail.com>
2021-03-27 07:44:55 +0800

    Add nn.m

--- vehicle/nn.m
+++ vehicle/nn.m
@@ -1,30 +1,43 @@
+T = ovehicleemission1;
 % raw Age
-rawfeaturesT = [T(:,9), T(:,11:22)];
-rawtargetsT = T(:,4:7);
+a = 14;
+b = 31;
+c = 61;
+t = 7;
+rawfeaturesT = [T(:,a), T(:,b:c)];
+rawtargetsT = T(:,t:t+3);
 rawfeatures = table2array(rawfeaturesT)';
 rawtargets = table2array(rawtargetsT)';
 
 % Age Mean Normalize
-mfeaturesT = [T(:,42), T(:,11:22)];
-mtargetsT = T(:,43:46);
+a = 62;
+t = a+1;
+mfeaturesT = [T(:,a), T(:,b:c)];
+mtargetsT = T(:,t:t+3);
 mfeatures = table2array(mfeaturesT)';
 mtargets = table2array(mtargetsT)';
 
 % Age MaxMin Normalize
-mmfeaturesT = [T(:,47), T(:,11:22)];
-mmtargetsT = T(:,48:51);
+a = a+5;
+t = a+1;
+mmfeaturesT = [T(:,a), T(:,b:c)];
+mmtargetsT = T(:,t:t+3);
 mmfeatures = table2array(mmfeaturesT)';
 mmtargets = table2array(mmtargetsT)';
 
 % Age across Normalizer
-afeaturesT = [T(:,52), T(:,11:22)];
-atargetsT = T(:,53:56);
+a = a+5;
+t = a+1;
+afeaturesT = [T(:,a), T(:,b:c)];
+atargetsT = T(:,t:t+3);
 afeatures = table2array(afeaturesT)';
 atargets = table2array(atargetsT)';
 
 % Age Normalizer
-nfeaturesT = [T(:,57), T(:,11:22)];
-ntargetsT = T(:,58:61);
+a = a+5;
+t = a+1;
+nfeaturesT = [T(:,a), T(:,b:c)];
+ntargetsT = T(:,t:t+3);
 nfeatures = table2array(nfeaturesT)';
 ntargets = table2array(ntargetsT)';
 
